6f21211bcdac6e9fb36f53612d1e2b6a865a7027
Naveenkumar Murugan <mndlsoft@gmail.com>
2024-08-15 20:42:28 +1000

    script changes

--- fetch_medium_articles.py
+++ fetch_medium_articles.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 
 medium_username = "naveenkumarmurugan"
 api_url = f"https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@{medium_username}"
@@ -15,6 +16,12 @@ for item in data['items']:
         "date": item['pubDate']
     })
 
+# Ensure the data directory exists
+data_directory = "data"
+if not os.path.exists(data_directory):
+    os.makedirs(data_directory)
+
 # Write the JSON data to your Hugo project's data directory
-with open("data/medium_articles.json", "w") as f:
+with open(os.path.join(data_directory, "medium_articles.json"), "w") as f:
     json.dump(articles, f, indent=2)
+
